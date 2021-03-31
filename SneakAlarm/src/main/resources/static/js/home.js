76623e1624d365c1cd0a447b88bf6fa13f99ed07
Jigu Kwak <k94earth@naver.com>
2021-04-01 05:41:28 +0900

    setTimeout시간 250으로 변경

--- SneakAlarm/src/main/resources/static/js/home.js
+++ SneakAlarm/src/main/resources/static/js/home.js
@@ -37,7 +37,7 @@ $('document').ready(function() {
 							getFlag = 0;
 						page++;
 						passFlag = 1;
-					}, 500);
+					}, 250);
 				},
 				error: function() {
 					alert('error! : getEndedProductCardList');
