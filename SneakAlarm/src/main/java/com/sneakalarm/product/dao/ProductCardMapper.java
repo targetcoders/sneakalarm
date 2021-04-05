1998ee81eb1137ecf1ff588c3931c40af074fe3f
Jigu Kwak <k94earth@naver.com>
2021-04-06 05:20:07 +0900

    검색 진행중 구분

--- SneakAlarm/src/main/java/com/sneakalarm/product/dao/ProductCardMapper.java
+++ SneakAlarm/src/main/java/com/sneakalarm/product/dao/ProductCardMapper.java
@@ -1,5 +1,6 @@
 package com.sneakalarm.product.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import com.sneakalarm.product.dto.ProductCardVO;
@@ -7,5 +8,5 @@ import com.sneakalarm.product.dto.ProductCardVO;
 @Mapper
 public interface ProductCardMapper {
   List<ProductCardVO> getProductCardList();
-  List<ProductCardVO> getProductCardListByKeyword(String keyword);
+  ArrayList<ProductCardVO> getProductCardListByKeyword(String keyword);
 }
