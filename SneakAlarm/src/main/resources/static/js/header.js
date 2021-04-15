e278c7ace052a30f2a9b8c5f3771fbe84dcd9712
Jigu Kwak <k94earth@naver.com>
2021-04-16 01:34:05 +0900

    Make navbar-basic-for-mobile-frontend
    mobile용 하단 고정 navbar frontend 구현

---
+++ SneakAlarm/src/main/resources/static/js/header.js
@@ -0,0 +1,16 @@
+function showNavBar(){
+    var windowWidth = $(window).width();
+    if(windowWidth < 768){
+        $('#nav-bottom-mobile').show();
+        $('#nav-bottom-mobile-div').show();
+        $('#nav-bottom-mobile-div-btn-today').show();
+        $('#nav-bottom-mobile-div-btn-news').show();
+    }
+    else {
+        $('#nav-bottom-mobile').hide();
+        $('#nav-bottom-mobile-div').hide();
+        $('#nav-bottom-mobile-div-btn-today').hide();
+        $('#nav-bottom-mobile-div-btn-news').hide();
+    }
+}
+
