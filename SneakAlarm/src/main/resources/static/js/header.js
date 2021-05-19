c5d01b2b71e67f53f0a258d35ba311ae204d07f8
earth-k <k94earth@naver.com>
2021-05-20 00:16:13 +0900

    Added css attribute: shadow-box to navbar when scrolling down.

--- SneakAlarm/src/main/resources/static/js/header.js
+++ SneakAlarm/src/main/resources/static/js/header.js
@@ -24,3 +24,27 @@ function toggleSearchBar(flag) {
     $('#searchBar').blur();
   }
 }
+
+function setShadowboxNavbar(){
+  if (getScrollTop() > 50) {
+    $('#top-navbar').css({'box-shadow': '0px 0px 10px 1.5px rgba(0,0,0,0.1)'});
+  } else {
+    $('#top-navbar').css({'box-shadow': '0px 0px 0px 0px rgba(0,0,0,0)'});
+  }
+  console.log(getScrollTop());
+}
+
+// 현재 스크롤한 높이를 구하는 함수
+function getScrollTop() {
+	return (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+}
+// 문서의 높이를 구하는 함수
+function getDocumentHeight() {
+	const body = document.body;
+	const html = document.documentElement;
+
+	return Math.max(
+		body.scrollHeight, body.offsetHeight,
+		html.clientHeight, html.scrollHeight, html.offsetHeight
+	);
+}
