16031dbfadf0653508d2907226fc835a09e9db70
earth-k <k94earth@naver.com>
2021-05-30 03:14:13 +0900

    RaffleSetting - Create
    - Add Junit4 dependency to pom.xml
    - Add Create feature for RaffleSetting

---
+++ SneakAlarm/src/main/java/com/sneakalarm/rafflesetting/domain/RaffleSetting.java
@@ -0,0 +1,43 @@
+package com.sneakalarm.rafflesetting.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class RaffleSetting {
+
+  private String id;
+  private String raffleSettingName;
+  private String storeName;
+  private String country;
+  private String delivery;
+  private String payType;
+  private String specialCase;
+  private String productPrice;
+  private String imgSrc;
+  private LocalDateTime startDateTime;
+  private LocalDateTime endDateTime;
+
+  public RaffleSetting() {
+  }
+
+  @Builder
+  public RaffleSetting(String id, String raffleSettingName, String storeName, String country,
+      String delivery, String payType, String specialCase, String productPrice, String imgSrc) {
+    this.id = id;
+    this.raffleSettingName = raffleSettingName;
+    this.storeName = storeName;
+    this.country = country;
+    this.delivery = delivery;
+    this.payType = payType;
+    this.specialCase = specialCase;
+    this.productPrice = productPrice;
+    this.imgSrc = imgSrc;
+  }
+
+}
