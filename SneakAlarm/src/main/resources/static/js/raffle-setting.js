012707909aad7b19258416c23cd9a8066c556d9e
earth-k <k94earth@naver.com>
2021-06-03 22:32:12 +0900

    Refactor raffle-setting.js
    - create RaffleSetting class

--- SneakAlarm/src/main/resources/static/js/raffle-setting.js
+++ SneakAlarm/src/main/resources/static/js/raffle-setting.js
@@ -7,7 +7,8 @@ $('document').ready(function() {
       for(let i in jsonData){
         console.log(jsonData[i]);
         let dateTime = getDateTimeString(jsonData[i].insertDateTime.date, jsonData[i].insertDateTime.time);
-        $('.raffle-setting-rows').append('<tr><td scope="row">'+i+'</td><td scope="row"><a href="/raffle-settings/detail?id='+jsonData[i].id+'">'+jsonData[i].raffleSettingName+'</a></td><td scope="row">'+dateTime+'</td></tr>');
+        let raffleSetting = new RaffleSetting(i, jsonData[i].id, jsonData[i].raffleSettingName, dateTime);
+        $('.raffle-setting-rows').append(raffleSetting.getRaffleSettingTableRow());
       }
     },
     error: function() {
@@ -15,3 +16,16 @@ $('document').ready(function() {
     }
   });
 });
+
+class RaffleSetting {
+  constructor(seq, id, name, insertDateTime){
+    this.seq = seq;
+    this.id = id;
+    this.name = name;
+    this.insertDateTime = insertDateTime;
+  }
+
+  getRaffleSettingTableRow() {
+    return '<tr><td scope="row">'+this.seq+'</td><td scope="row"><a href="/raffle-settings/detail?id='+this.id+'">'+this.name+'</a></td><td scope="row">'+this.insertDateTime+'</td></tr>';
+  }
+}
