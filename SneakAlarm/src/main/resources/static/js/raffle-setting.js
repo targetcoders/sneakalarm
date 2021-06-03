08e85187b863cc98ea38e826839ddc3ded5293d4
earth-k <k94earth@naver.com>
2021-06-04 00:55:23 +0900

    create raffle-setting-detail page
    - create raffle-setting-detail page
    - refactor raffle-setting.js
    - refactor raffle-setting-detail.js

--- SneakAlarm/src/main/resources/static/js/raffle-setting.js
+++ SneakAlarm/src/main/resources/static/js/raffle-setting.js
@@ -7,8 +7,8 @@ $('document').ready(function() {
       for(let i in jsonData){
         console.log(jsonData[i]);
         let dateTime = getDateTimeString(jsonData[i].insertDateTime.date, jsonData[i].insertDateTime.time);
-        let raffleSetting = new RaffleSetting(i, jsonData[i].id, jsonData[i].raffleSettingName, dateTime);
-        $('.raffle-setting-rows').append(raffleSetting.getRaffleSettingTableRow());
+        let raffleSettingTableRow = new RaffleSettingTableRow(i, jsonData[i].id, jsonData[i].raffleSettingName, dateTime);
+        $('.raffle-setting-rows').append(raffleSettingTableRow.getTableRow());
       }
     },
     error: function() {
@@ -17,7 +17,8 @@ $('document').ready(function() {
   });
 });
 
-class RaffleSetting {
+class RaffleSettingTableRow {
+
   constructor(seq, id, name, insertDateTime){
     this.seq = seq;
     this.id = id;
@@ -25,7 +26,80 @@ class RaffleSetting {
     this.insertDateTime = insertDateTime;
   }
 
-  getRaffleSettingTableRow() {
-    return '<tr><td scope="row">'+this.seq+'</td><td scope="row"><a href="/raffle-settings/detail?id='+this.id+'">'+this.name+'</a></td><td scope="row">'+this.insertDateTime+'</td></tr>';
+  getTableRow() {
+    return '<tr><td scope="row">'+this.seq+'</td><td scope="row"><a href="/raffle-setting/detail/'+this.id+'">'+this.name+'</a></td><td scope="row">'+this.insertDateTime+'</td></tr>';
+  }
+}
+
+class RaffleSetting {
+
+  constructor(id, raffleSettingName, storeName, country, delivery, payType, specialCase, productPrice, imgSrc, startDateTime, endDateTime, insertDateTime){
+    this.id = id;
+    this.raffleSettingName = raffleSettingName;
+    this.storeName = storeName;
+    this.country = country;
+    this.delivery = delivery;
+    this.payType = payType;
+    this.specialCase = specialCase;
+    this.productPrice = productPrice;
+    this.imgSrc = imgSrc;
+    this.startDateTime = startDateTime;
+    this.endDateTime = endDateTime;
+    this.insertDateTime = insertDateTime;
+  }
+
+  getRaffleSettingDetail(){
+    let divTag = $('<div class="raffle-setting-detail"></div>');
+    divTag.append(this.getDivId());
+    divTag.append(this.getDivRaffleSettingName());
+    divTag.append(this.getDivStoreName());
+    divTag.append(this.getDivCountry());
+    divTag.append(this.getDivDelivery());
+    divTag.append(this.getDivPayType());
+    divTag.append(this.getDivSpecialCase());
+    divTag.append(this.getDivProductPrice());
+    divTag.append(this.getDivImgSrc());
+    divTag.append(this.getDivStartDateTime());
+    divTag.append(this.getDivEndDateTime());
+    divTag.append(this.getDivInsertDateTime());
+
+    return divTag;
+  }
+
+  getDivId(){
+    return '<div>id: '+this.id+'</div>';
+  }
+  getDivRaffleSettingName(){
+      return '<div>raffleSettingName: '+this.raffleSettingName+'</div>';
+  }
+  getDivStoreName(){
+      return '<div>storeName: '+this.storeName+'</div>';
+  }
+  getDivCountry(){
+        return '<div>country: '+this.country+'</div>';
+  }
+  getDivDelivery(){
+        return '<div>delivery: '+this.delivery+'</div>';
+  }
+  getDivPayType(){
+        return '<div>payType: '+this.payType+'</div>';
+  }
+  getDivSpecialCase(){
+        return '<div>specialCase: '+this.specialCase+'</div>';
+  }
+  getDivProductPrice(){
+        return '<div>productPrice: '+this.productPrice+'</div>';
+  }
+  getDivImgSrc(){
+        return '<div>imgSrc: '+this.imgSrc+'</div>';
+  }
+  getDivStartDateTime(){
+        return '<div>startDateTime: '+this.startDateTime+'</div>';
+  }
+  getDivEndDateTime(){
+        return '<div>endDateTime: '+this.endDateTime+'</div>';
+  }
+  getDivInsertDateTime(){
+        return '<div>insertDateTime: '+this.insertDateTime+'</div>';
   }
 }
