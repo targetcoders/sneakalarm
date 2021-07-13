2bd9697c22c7faced2e2490bddedb026d7654d2b
earth-k <k94earth@naver.com>
2021-07-14 02:51:51 +0900

    instagram feed text 생성 내용 및 방식 수정

--- SneakAlarm/src/main/java/com/sneakalarm/raffle/domain/InstaFeed.java
+++ SneakAlarm/src/main/java/com/sneakalarm/raffle/domain/InstaFeed.java
@@ -23,17 +23,48 @@ public class InstaFeed {
 
   private String generateFeedText() throws ParseException {
     return "⏰"+raffleVO.getStoreName()+" "+raffleVO.getRaffleType()+": ~ "
-        + textForTodayOrTomorrow(raffleVO.getEndDate())+" "+raffleVO.getEndTime()+"\n"+raffleVO.getModel_kr()+commonText();
+        + textForTodayOrTomorrow(raffleVO.getEndDate())+" "+raffleVO.getEndTime()
+        + "\n" + raffleVO.getModel_kr()
+        + "\n\n" + announceText()
+        + "\n\n\n" + specialCaseText(raffleVO.getSpecialCase())
+        + "\n\n\n" + commonText();
+  }
+
+  private String specialCaseText(String specialCase) {
+    if(specialCase == null)
+      return "";
+
+    return "✅ 주의 사항 ✅"
+        +"\n" + specialCase;
+  }
+
+  private String announceText(){
+    String result = raffleVO.getStoreName()
+        + "에서 "+ raffleVO.getRaffleType() +" 진행합니다!!\uD83D\uDD25\n"
+        + "기회를 놓치지 마세요!! \uD83D\uDD25";
+
+    if(raffleVO.getRaffleType().equals("응모")) {
+      result += "\n여러분의 당첨을 기원합니다.\uD83E\uDD17";
+    }
+
+    return result;
   }
 
   private String commonText() {
-    return "\n\n@sneakalarm.com⬅응모 링크"
+    return
+        "[편하게 응모하는 방법1]\n"
+        + "1. @sneakalarm 프로필로 이동\n"
+        + "2. 홈페이지 주소를 클릭\uD83D\uDE0D\n"
+        + "3. 참여할 응모 카드를 클릭"
         + "\n\n"
-        + "\" 게시물 \uD83D\uDECE알람 설정하고 한정판 신발 겟하세요! \"\n"
-        + "\uD83D\uDCC8시세:　@sneakalarm_justdidit\n"
-        + "\uD83D\uDEA8뉴스:　@sneakalarm_news\n"
-        + "\n"
-        + "스알님들의 좋아요+댓글은 큰 도움이 됩니다\uD83D\uDC4D\uD83C\uDFFB\n"
+        + "[편하게 응모하는 방법2]\n"
+        + "1. 브라우저 실행\n"
+        + "2. 주소창에 sneakalarm.com 입력\n"
+        + "3. 참여할 응모 카드를 클릭"
+        + "\n\n"
+        + "\" 게시물 \uD83D\uDECE알람 설정하고 한정판 신발 겟하세요! \""
+        + "\n\n"
+        + "스알님들의 좋아요+댓글은 큰 힘이 됩니다\uD83D\uDC4D\uD83C\uDFFB"
         + "\n"
         + ".\n"
         + ".\n"
