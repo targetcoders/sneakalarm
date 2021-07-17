60f4efe851298d8a8420425d68a2f21bd405812e
earth-k <k94earth@naver.com>
2021-07-17 23:59:06 +0900

    get .site_card elements test

--- SneakAlarm/src/main/java/com/sneakalarm/raffle/controller/RaffleAutoInsertController.java
+++ SneakAlarm/src/main/java/com/sneakalarm/raffle/controller/RaffleAutoInsertController.java
@@ -3,6 +3,8 @@ package com.sneakalarm.raffle.controller;
 import java.io.IOException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -31,8 +33,17 @@ public class RaffleAutoInsertController {
   @PostMapping("/raffle-auto-insert/{productId}")
   public ResponseEntity<String> autoInsertRaffle(@RequestParam("url") String url,
       @PathVariable("productId") String productId) throws IOException {
-    Document luckyDrawHTML = Jsoup.connect(url).get();
-    System.out.println(luckyDrawHTML);
+        Document luckyDrawHTML = Jsoup.connect(url)
+        .userAgent("Mozilla/5.0 (Windows; U; WindowsNT 5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+        .referrer("http://www.google.com")
+        .get();
+
+        Elements siteCardList = luckyDrawHTML.select("div.site_card");
+        for(Element e : siteCardList) {
+          System.out.println(e);
+        }
+
+    //System.out.println(luckyDrawHTML);
     return new ResponseEntity<>("{\"url\":\""+url+"\"}", HttpStatus.OK);
   }
 
