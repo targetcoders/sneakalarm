84447e40658c68e604baa84f99e4b74c0fb93a48
earth-k <k94earth@naver.com>
2021-07-17 23:20:31 +0900

    create raffle auto insert controller and interaction test

---
+++ SneakAlarm/src/main/java/com/sneakalarm/raffle/controller/RaffleAutoInsertController.java
@@ -0,0 +1,35 @@
+package com.sneakalarm.raffle.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class RaffleAutoInsertController {
+
+  @GetMapping("/raffle-auto-insert")
+  public String showRaffleAutoInsertView(@RequestParam(name = "productId") String productId,
+      @RequestParam(name = "productName") String productName, Model model) {
+    String tempProductName = productName;
+    String tempProductId = productId;
+    model.addAttribute("productName", tempProductName);
+    model.addAttribute("productId", tempProductId);
+
+    return "views/raffle/auto-addition";
+  }
+
+  @ResponseBody
+  @PostMapping("/raffle-auto-insert/{productId}")
+  public ResponseEntity<String> autoInsertRaffle(@RequestParam("url") String url,
+      @PathVariable("productId") String productId) {
+
+    return new ResponseEntity<>("{\"url\":\""+url+"\"}", HttpStatus.OK);
+  }
+
+}
---
+++ SneakAlarm/src/main/resources/static/js/raffle/auto-addition.js
@@ -0,0 +1,21 @@
+$('document').ready(function(){
+  $('#auto-insert-submit').on('click',function(){
+    let productId = $('#productId').text();
+    let url = $('#target-url').val();
+    let storeName = $('#target-store-name').val();
+    $.ajax({
+      type: 'POST',
+      url: '/raffle-auto-insert/'+'productId',
+      data: {
+        url: url,
+        storeName: storeName
+      },
+      success: function(storeName){
+        alert('['+storeName+'] 응모 카드가 등록되었습니다.');
+      },
+      error: function(errorMessage){
+        alert('응모 카드 등록에 실패하였습니다.\n'+'errorMessage');
+      }
+    });
+  });
+});
