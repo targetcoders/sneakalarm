93f5522b9d3810a4279af45366633188cb057698
earth-k <k94earth@naver.com>
2021-07-18 23:41:01 +0900

    배대지, 직배인 경우 raffle-auto-insert 예외처리

--- SneakAlarm/src/main/java/com/sneakalarm/raffle/service/RaffleAutoInsertServiceImpl.java
+++ SneakAlarm/src/main/java/com/sneakalarm/raffle/service/RaffleAutoInsertServiceImpl.java
@@ -36,12 +36,16 @@ public class RaffleAutoInsertServiceImpl implements RaffleAutoInsertService{
       String raffleUrl = siteCardParser.getRaffleUrl(e);
       String raffleEndDateTime = siteCardParser.getRaffleEndDateTime(e);
 
+      delivery = changeValueFor(delivery);
+
       String prefixDelivery = (delivery==null) ? "[온라인 구매]" : "["+delivery+"]";
       List<RaffleSetting> raffleSettingList = raffleSettingService
           .getRaffleSettingByKeyword(prefixDelivery+storeName.split(" ")[0]);
-      if(raffleSettingList.isEmpty()){
 
+      if(raffleSettingList.isEmpty()){
+        throw new IllegalStateException("RaffleSetting 테이블에 일치하는 데이터가 없습니다.");
       }
+
       RaffleSetting raffleSetting = raffleSettingList.get(0);
       DateTime dateTime = new DateTimeImpl();
       SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm");
@@ -51,7 +55,7 @@ public class RaffleAutoInsertServiceImpl implements RaffleAutoInsertService{
       }
       RaffleVO raffleVO = RaffleVO.builder()
           .productId(productId)
-          .storeName(storeName)
+          .storeName(raffleSetting.getStoreName())
           .url(raffleUrl)
           .raffleType(raffleSetting.getRaffleType())
           .country(raffleSetting.getCountry())
@@ -72,5 +76,12 @@ public class RaffleAutoInsertServiceImpl implements RaffleAutoInsertService{
       raffleSettingService.insertRaffle(raffleVO);
     }
     return raffleVOList;
+  }
+
+  private String changeValueFor(String delivery) {
+    if(delivery != null && (delivery.equals("배대지") || delivery.equals("직배"))){
+      delivery = null;
+    }
+    return delivery;
   }
 }
