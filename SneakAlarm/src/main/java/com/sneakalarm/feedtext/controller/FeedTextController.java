5aabf443c510db1b69d763b0c0c96dcbecddac10
earth-k <k94earth@naver.com>
2021-07-29 08:46:22 +0900

    feed-text endTime 초 단위 제거, 제품명 들어가지 않는 문제 해결 및 피드백 반영

--- SneakAlarm/src/main/java/com/sneakalarm/feedtext/controller/FeedTextController.java
+++ SneakAlarm/src/main/java/com/sneakalarm/feedtext/controller/FeedTextController.java
@@ -1,5 +1,7 @@
 package com.sneakalarm.feedtext.controller;
 
+import com.sneakalarm.product.dto.ProductVO;
+import com.sneakalarm.product.service.ProductService;
 import com.sneakalarm.raffle.domain.DateTimeImpl;
 import com.sneakalarm.raffle.domain.InstaFeed;
 import com.sneakalarm.raffle.dto.RaffleVO;
@@ -17,12 +19,20 @@ public class FeedTextController {
 
   @Autowired
   RaffleService raffleService;
+  @Autowired
+  ProductService productService;
 
   @GetMapping("/feed-text/{raffleId}")
   public String getFeedTextPage(@PathVariable("raffleId") String raffleId, Model model)
       throws ParseException {
     List<RaffleVO> raffleVOList = raffleService.getRaffleList(raffleId);
+    RaffleVO raffleVO = raffleVOList.get(0);
+    if(raffleVO.getModel_kr() == null || raffleVO.getModel_kr().isEmpty()) {
+      List<ProductVO> productVOList = productService.getProductList(raffleVO.getProductId());
+      raffleVO.setModel_kr(productVOList.get(0).getModel_kr());
+    }
     InstaFeed instaFeed = new InstaFeed(raffleVOList.get(0), new DateTimeImpl());
+
     model.addAttribute("feedText",instaFeed.getText());
     model.addAttribute("raffleVO",raffleVOList.get(0));
     return "views/feed-text/feed-text";
