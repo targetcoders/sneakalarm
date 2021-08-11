17faf9a3ba8e0cc8ba1ea6fcb549e5f48a11fd06
earth-k <k94earth@naver.com>
2021-08-11 23:15:54 +0900

    GET /now/draw-list/agent 배대지 API controller 구현

--- SneakAlarm/src/main/java/com/sneakalarm/util/Week.java
+++ SneakAlarm/src/main/java/com/sneakalarm/util/Week.java
@@ -2,7 +2,6 @@ package com.sneakalarm.util;
 
 import java.util.Calendar;
 import java.util.Date;
-import org.springframework.stereotype.Component;
 
 public class Week {
   private final Date date;
