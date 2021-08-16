7f035298435a5ef4e983e0325c34b77438ce19bf
earth-k <k94earth@naver.com>
2021-08-17 01:52:26 +0900

    카테고리 팝업 창이 꺼졌는데 로고가 흰색으로 유지되는 버그 수정

--- SneakAlarm/src/main/resources/static/js/today.js
+++ SneakAlarm/src/main/resources/static/js/today.js
@@ -20,8 +20,6 @@ function setPopup(){
     $('.fixed-bottom').css('z-index',1028);
   });
   $('#category-close-btn, .popBg').on('click',function(){
-    $('.navbar__clock__date').css('color','black');
-    $('.navbar__clock__week').css('color','black');
     $('.navbar__bogo').attr('src','image/pc/SneakAlarm_white_870x180.png');
     $('.navbar-top--mobile__container').css('background-color','rgba(0,0,0,0)');
     $('.navbar-top--pc__container').css('background-color','rgba(255,255,255,0.9)');
@@ -29,6 +27,8 @@ function setPopup(){
   });
 }
 function closeCategoryPopUp(){
+  $('.navbar__clock__date').css('color','black');
+  $('.navbar__clock__week').css('color','black');
   $('.fixed-bottom').show();
   $('#category-list-popup').fadeOut(200);
 }
